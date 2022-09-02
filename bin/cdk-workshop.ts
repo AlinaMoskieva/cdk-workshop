96ca8b06cff49775f8446ee585e22757e861e021
Alina Moskieva <alina.moskieva@flatstack.com>
2022-09-02 11:43:07 +0200

    Slack bot

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,21 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack.js';
+
+if (!process.env['DIL_CDK_SCOPE']) {
+    throw new Error('DIL_CDK_SCOPE environment variable is not set');
+}
 
 const app = new cdk.App();
 new CdkWorkshopStack(app, 'CdkWorkshopStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    stackName: `${process.env['DIL_CDK_SCOPE']}-cdk-workshop-stack`,
+    scope: process.env['DIL_CDK_SCOPE'],
 });
